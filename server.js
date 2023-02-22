b96005d3cc7ae727601464ed0301bd30cb4d4a80
[mick lito] <[micklito.dev@gmail.com]>
2023-02-21 23:45:16 -0500

    V1.4.3
    updated cors requirements in server.js and contact.tsx

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 app.post("/", (req, res) => {
   console.log(req.body);
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://dynamic-portfolio-ten.vercel.app");
   res.send();
 });
 
