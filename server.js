1bc761d636bac417a1937409bfb7c892ff30eddd
[mick lito] <[micklito.dev@gmail.com]>
2023-02-22 00:44:14 -0500

    1.4.7

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 app.post("/", (req, res) => {
   console.log(req.body);
-  res.setHeader("Access-Control-Allow-Origin", "https://dynamic-portfolio-ten.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "https://dynamic-portfolio-ten.vercel.app");
   res.send();
 });
 
