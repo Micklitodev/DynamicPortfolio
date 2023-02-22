13e2f144c58791f5e8aa66eced5b2f754a8b3a3e
[mick lito] <[micklito.dev@gmail.com]>
2023-02-22 00:34:52 -0500

    push

--- next.config.js
+++ next.config.js
@@ -9,7 +9,8 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/https://dynamic-portfolio-ten.vercel.app/",
+        // matching all API routes
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
