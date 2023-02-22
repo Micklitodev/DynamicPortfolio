b7a799b21e271f0d9a8e14d8c03f50e3b2dfd925
[mick lito] <[micklito.dev@gmail.com]>
2023-02-22 15:17:43 -0500

    1.4.10

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const router = require('express-router')
 
 app.use(express.json());
 app.use(cors());
-
-const router = require('express-router')
-
 app.use(router)
 
 router.post("/", (req, res) => {
