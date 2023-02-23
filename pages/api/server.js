fbc888f87f28d3e0d995c419d2bb6fdae8c64735
[mick lito] <[micklito.dev@gmail.com]>
2023-02-23 01:04:54 -0500

    1.4.16

--- pages/api/server.js
+++ pages/api/server.js
@@ -3,17 +3,15 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-console.log(PORT)
-
 app.use(express.json());
 app.use(cors());
 
-app.post("/send", (req, res) => {
+app.post("/", (req, res) => {
   console.log(req.body);
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200).send("Received a POST HTTP request");
 });
 
 app.listen(PORT, () => {
   console.log(`the server is listening on port ${PORT}`);
 });
+
