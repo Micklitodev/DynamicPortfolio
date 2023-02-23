62dd7ad55c5c1f2fb274b3a30f8535182db47cdd
[mick lito] <[micklito.dev@gmail.com]>
2023-02-23 00:02:11 -0500

    1.4.15

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.post("/api/contact", (req, res) => {
+app.get("/api/contact", (req, res) => {
   console.log(req.body);
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200);
