4231f9dca06a6be58181de01e8d30e26461cbac5
[mick lito] <[micklito.dev@gmail.com]>
2023-02-22 23:42:07 -0500

    1.4.11
    updated server.js to work inside of deployment.

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const router = require('express-router')
 
 app.use(express.json());
 app.use(cors());
-app.use(router)
 
-router.post("/", (req, res) => {
+app.post("*", (req, res) => {
   console.log(req.body);
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.send(); 
+  res.status(200);
 });
 
 app.listen(PORT, () => {
