1fa1b2ab82a262f481c682dba51664050bb6c34f
[mick lito] <[micklito.dev@gmail.com]>
2023-02-23 01:08:20 -0500

    1.4.7

--- pages/api/server.js
+++ pages/api/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.post("/", (req, res) => {
+app.post("/api/cotact", (req, res) => {
   console.log(req.body);
   res.status(200).send("Received a POST HTTP request");
 });
