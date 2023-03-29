c9bc2a8d06e891ca5830813bf3b077d934ce79aa
[mick lito] <[micklito.dev@gmail.com]>
2023-03-29 03:12:34 -0400

    2.2.1
    change to about me component styling.

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ const Home = (): JSX.Element => {
       <br />
       <main>
         <ScrollReveal.div animation="slide-in-bottom">
-          <div className={styles.container}>
+          <div className={styles.container} style={{height: '100vh'}}>
             <AboutMe />
           </div>
         </ScrollReveal.div>
