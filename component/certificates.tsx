9d7fcacc979d33bf17cce86108f4387dd64cb6ac
Mick L <90425599+Micklitodev@users.noreply.github.com>
2023-09-28 18:28:22 -0400

    4.6.0
    added new slide package that looks nicer and has more utility.

--- component/certificates.tsx
+++ component/certificates.tsx
@@ -1,30 +1,7 @@
 import React from "react";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import styles from "@/styles/Home.module.css";
-
-const spanStyle: object = {
-  padding: 8,
-  borderRadius: 5,
-  background: "rgb(39 39 42)",
-  color: "rgb(209 213 219)",
-  position: "absolute",
-  top: 30,
-  fontSize: 14,
-};
-
-const divStyle: object = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  borderRadius: "10px",
-  maxHeight: 450,
-  maxWidth: 570,
-  backgroundSize: "cover",
-  backgroundPosition: "center",
-  overflow: "hidden",
-  margin: "0 auto",
-};
 
 type slideobj = [
   {
@@ -48,22 +25,6 @@ const slideImages: slideobj = [
   },
 ];
 
-const arrowStyle: object = {
-  position: "absolute",
-  top: "50%",
-  transform: "translateY(-50%)",
-  width: "40px",
-  height: "40px",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  color: "rgba(240, 240, 240, 0.8)",
-  fontSize: "35px",
-  cursor: "pointer",
-  marginLeft: "20%",
-  marginRight: "20%",
-};
-
 const windowStyle: object = {
   backgroundColor: "rgba(15, 15, 17, .8)",
   backgroundRepeat: "no-repeat",
@@ -72,68 +33,38 @@ const windowStyle: object = {
 };
 
 const CertSlideShow = (): JSX.Element => {
-  const arrowOffset = "10px";
-
   return (
     <>
       <div
         style={{
           ...windowStyle,
         }}
-        className="h-screen"
+        className="flex items-center justify-center h-screen"
       >
-        <div id="certs">
-          <br />
+        <div id="certs" className="text-center">
           <h2 className={styles.componenth2darkbg}>My Certifications</h2>
           <br />
-          <Slide
-            prevArrow={
-              <div
-                style={{
-                  ...arrowStyle,
-                  left: `calc(${arrowOffset} - 20px)`,
-                }}
-              >
-                {"<"}
-              </div>
-            }
-            nextArrow={
-              <div
-                style={{
-                  ...arrowStyle,
-                  right: `calc(${arrowOffset} - 20px)`,
-                }}
-              >
-                {">"}
-              </div>
-            }
-          >
-            {slideImages.map((slideImage, index: number) => (
-              <div key={index}>
-                <div
-                  style={{
-                    ...divStyle,
-                    backgroundImage: `url(${slideImage.url})`,
-                  }}
-                >
-                  <span style={spanStyle}>{slideImage.caption}</span>
-                  <img
-                    src={slideImage.url}
-                    alt={slideImage.caption}
-                    style={{
-                      width: "100%",
-                      height: "auto",
-                      borderRadius: "20px",
-                    }}
-                  />
-                </div>
-              </div>
-            ))}
-          </Slide>
+          <div className="flex justify-center">
+            <div className="" style={{ maxWidth: 780 }}>
+              <Carousel showArrows={true} infiniteLoop={true}>
+                {slideImages.map((slideImage, index: number) => (
+                  <div key={index}>
+                    <img
+                      src={`${slideImage.url}`}
+                      alt={`Product Image ${index + 1}`}
+                      className="aspect-[1/1] object-cover object-center rounded-md"
+                      style={{ maxHeight: 500, maxWidth: 700 }}
+                    />
+                  </div>
+                ))}
+              </Carousel>
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
 };
+
 
 export default CertSlideShow;
